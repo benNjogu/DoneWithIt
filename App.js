c367660ebd284b0ceb1c8303d11657ebf9f85b6c
benNjogu <kamandemachafi@gmail.com>
2022-11-16 09:00:06 +0300

    Alert part 2

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView, Button } from "react-native";
+import { StyleSheet, SafeAreaView, Alert, Button } from "react-native";
 
 export default function App() {
   return (
@@ -7,7 +7,12 @@ export default function App() {
       <Button
         color={"orange"}
         title="Click Me"
-        onPress={() => alert("Button Tapped")}
+        onPress={() =>
+          Alert.alert("My title", "My Message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
       />
     </SafeAreaView>
   );
