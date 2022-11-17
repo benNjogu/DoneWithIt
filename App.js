872d008f11ec74f71b28b1b547883b9a755b6180
benNjogu <kamandemachafi@gmail.com>
2022-11-17 04:53:46 +0300

    Flexbasis and FlexGrow

--- App.js
+++ App.js
@@ -19,7 +19,9 @@ export default function App() {
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: 100,
+          flexBasis: 100,
+          flexGrow: 1,
+          //width: 100,
           height: 100,
         }}
       />
@@ -32,21 +34,7 @@ export default function App() {
       />
       <View
         style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
           backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
           width: 100,
           height: 100,
         }}
