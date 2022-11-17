e5659c13b27181ac69977daacb2d1e7d74aff5e8
benNjogu <kamandemachafi@gmail.com>
2022-11-17 04:26:04 +0300

    Flexbox direction

--- App.js
+++ App.js
@@ -8,25 +8,29 @@ export default function App() {
         {
           backgroundColor: "#fff",
           flex: 1,
+          flexDirection: "row",
         },
       ]}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          flex: 2,
+          width: 100,
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
-          flex: 1,
+          width: 100,
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
-          flex: 1,
+          width: 100,
+          height: 100,
         }}
       />
     </View>
