13ec3bb0f3a1aae18b25be9eb6f5fdc43866ff25
benNjogu <kamandemachafi@gmail.com>
2022-11-17 04:46:05 +0300

    Flexwrap and alignContent

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ export default function App() {
           flexDirection: "row",
           justifyContent: "center", //main/primary axis
           alignItems: "center", //secondary axis
+          alignContent: "center",
+          flexWrap: "wrap",
         },
       ]}
     >
@@ -18,20 +20,33 @@ export default function App() {
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 300,
-          alignSelf: "flex-start",
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
           width: 100,
-          height: 200,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "grey",
+          width: 100,
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "greenyellow",
           width: 100,
           height: 100,
         }}
