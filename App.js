0a67b462bdf4e054d0b663ffbf99e2c1328c05e5
benNjogu <kamandemachafi@gmail.com>
2022-11-17 06:50:30 +0300

    Welcome and ViewImage screens

--- App.js
+++ App.js
@@ -1,51 +1,8 @@
 import { StyleSheet, StatusBar, View, Platform } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View
-      style={[
-        styles.container,
-        {
-          backgroundColor: "#fff",
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "center", //main/primary axis
-          alignItems: "center", //secondary axis
-        },
-      ]}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-          position: "absolute",
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
