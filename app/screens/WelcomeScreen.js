b7ac8e95a027ac2f5aa84619c421cf582f65f2aa
benNjogu <kamandemachafi@gmail.com>
2022-11-17 19:20:45 +0300

    Borders part 1

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 
+import colors from "../../config/colors";
+
 function WelcomeScreen(props) {
   return (
     <ImageBackground
@@ -29,7 +31,7 @@ const styles = StyleSheet.create({
   loginButton: {
     width: "100%",
     height: 70,
-    backgroundColor: "#fc5c65",
+    backgroundColor: colors.primary,
   },
   logo: {
     width: 100,
@@ -43,7 +45,7 @@ const styles = StyleSheet.create({
   registerButton: {
     width: "100%",
     height: 70,
-    backgroundColor: "#4ecdc4",
+    backgroundColor: colors.secondary,
   },
 });
 
