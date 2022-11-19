d0bfd157d04cb70fde8cc227ed4e4631eb226df8
benNjogu <kamandemachafi@gmail.com>
2022-11-19 13:46:40 +0300

    Building the listingDetailsScreen

--- App.js
+++ App.js
@@ -2,21 +2,8 @@ import { View } from "react-native";
 import { React } from "react";
 
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f8f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red jacket for sale!"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
