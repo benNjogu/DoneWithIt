f72f34771f312496a6cebc39116735bb7a2a1e47
benNjogu <kamandemachafi@gmail.com>
2022-11-20 23:15:23 +0300

    Extending the list item component

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { React } from "react";
 
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 
 export default function App() {
   return (
     <Screen>
-      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+      <ListItem title="My title" ImageComponent={<Icon name="email" />} />
     </Screen>
   );
 }
