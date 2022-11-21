0d06e00df762faa93c0e194d41072157f4633c98
benNjogu <kamandemachafi@gmail.com>
2022-11-21 16:37:36 +0300

    TextInput

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
-import { React } from "react";
+import React, { useState } from "react";
+import { TextInput, Text } from "react-native";
 
-import ListingsScreen from "./app/screens/ListingsScreen";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen style={{ marginTop: 8 }}>
+      <Text>{firstName}</Text>
+      <TextInput
+        secureTextEntry={true}
+        clearButtonMode="always" //ios only
+        keyboardType="numeric"
+        maxLength={5}
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="First Name"
+        style={{ borderBottomColor: "#ccc", borderBottomWidth: 1 }}
+      />
+    </Screen>
+  );
 }
