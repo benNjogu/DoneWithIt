5b750e5a3bcc8de6f803fe0f66df8a757392319f
benNjogu <kamandemachafi@gmail.com>
2022-11-23 19:17:27 +0300

    Building the ListingEditScreen

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Screen from "./app/components/Screen";
 import LoginScreen from "./app/screens/LoginScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <ListingEditScreen />;
 }
