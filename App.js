5a22130220fea84b7e613679d844bc749b16f93a
benNjogu <kamandemachafi@gmail.com>
2022-11-25 10:35:47 +0300

    Requesting permissions

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
+import AppText from "./app/components/AppText";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted)
+      alert("You need to enable permissions to access the library.");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  return (
+    <Screen>
+      <AppText>Hello</AppText>
+    </Screen>
+  );
 }
