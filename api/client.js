8f9e752bb645fa2a96095f24e86822ff4d791000
benNjogu <kamandemachafi@gmail.com>
2022-11-28 05:39:03 +0300

    Creating an API layer

--- api/client.js
+++ api/client.js
@@ -5,8 +5,4 @@ const apiClient = create({
   baseURL: "http://192.168.43.81:9000/api",
 });
 
-apiClient.get("/listings").then((response) => {
-  if (!response.ok) {
-    response.problem;
-  }
-});
+export default apiClient;
