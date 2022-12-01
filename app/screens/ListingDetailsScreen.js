c406476e0ea399dda2d3881947d8bc291a61cd89
benNjogu <kamandemachafi@gmail.com>
2022-12-01 16:54:39 +0300

    Fixing listItem bug

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -10,31 +10,31 @@ function ListingDetailsScreen({ route }) {
   const listing = route.params;
 
   return (
-    <View>
-      <Image
-        style={styles.image}
-        preview={{ uri: listing.images[0].thumbnailUrl }}
-        tint="light"
-        uri={listing.images[0].url}
-      />
-      <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>{listing.title}</AppText>
-        <AppText style={styles.price}>${listing.price} </AppText>
-        <View style={styles.userContainer}>
-          <ListItem
-            image={require("../assets/mosh.jpg")}
-            title="Bernad Macabi"
-            subTitle="5 listings"
-          />
+    <>
+      <View>
+        <Image
+          style={styles.image}
+          preview={{ uri: listing.images[0].thumbnailUrl }}
+          tint="light"
+          uri={listing.images[0].url}
+        />
+        <View style={styles.detailsContainer}>
+          <AppText style={styles.title}>{listing.title}</AppText>
+          <AppText style={styles.price}>${listing.price} </AppText>
         </View>
       </View>
-    </View>
+      <ListItem
+        image={require("../assets/mosh.jpg")}
+        title="Bernad Macabi"
+        subTitle="5 listings"
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   detailsContainer: {
-    padding: 20,
+    padding: 16,
   },
   image: {
     width: "100%",
@@ -44,14 +44,14 @@ const styles = StyleSheet.create({
     color: colors.secondary,
     fontWeight: "bold",
     fontSize: 20,
-    marginVertical: 10,
+    marginVertical: 8,
   },
   title: {
     fontSize: 24,
     fontWeight: "500",
   },
   userContainer: {
-    marginVertical: 40,
+    marginVertical: 24,
   },
 });
 
