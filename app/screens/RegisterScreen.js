5547e70d7cff2b4c83db6df2e0b8af625db6ba8c
benNjogu <kamandemachafi@gmail.com>
2022-12-03 12:19:45 +0300

    Adding register screen

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,20 +1,57 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
+import * as Yup from "yup";
 
-import AppText from "../components/AppText";
+import Screen from "../components/Screen";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 
-function RegisterScreen(props) {
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label("Name"),
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
+function RegisterScreen() {
   return (
-    <View style={styles.container}>
-      <AppText>Register</AppText>
-    </View>
+    <Screen style={styles.container}>
+      <AppForm
+        initialValues={{ name: "", email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          autoCorrect={false}
+          icon="account"
+          name="name"
+          placeholder="Name"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          keyboardType="email-address"
+          name="email"
+          placeholder="Email"
+          textContentType="emailAddress"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          secureTextEntry
+          textContentType="password"
+        />
+        <SubmitButton title="Register" />
+      </AppForm>
+    </Screen>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignContent: "center",
-    alignItems: "center",
+    padding: 8,
   },
 });
 
